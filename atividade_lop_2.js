aabc7fac859e6b3a0b8aeb40266860b940a2a1b0
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-03-11 21:45:06 -0300

    Update atividade_lop_2.js

--- atividade_lop_2.js
+++ atividade_lop_2.js
@@ -1,3 +1,6 @@
-var nome = prompt("qual é o seu nome?");
-var idade = prompt("qual é a sua idade?");
+// Atividade de logíca da programação, 11/03/2024, Escola de ciências & tecnologia
+
+let nome = prompt("qual é o seu nome?");
+let idade = prompt("qual é a sua idade?");
+
 console.log(nome + idade + "anos");
