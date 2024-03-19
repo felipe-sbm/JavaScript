12c7992a719739ddb806a483ab517830fda07346
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-03-18 21:19:16 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,3 +1,5 @@
+// Atividade de logíca da programação, 11/03/2024, Escola de ciências & tecnologia
+
 let massa = parseFloat(prompt("Informe a sua massa em quilogramas:"));
 let altura = parseFloat(prompt("Informe a sua altura em metros"));
 
