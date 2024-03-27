1c1ce5e146c28d661f2db6898ec416beb4effa60
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-03-27 15:02:33 -0300

    Create atividade_lop_4.js

---
+++ CeT/charpter_1/atividade_lop_4.js
@@ -0,0 +1,32 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 15/03/2024. ECT - UFRN
+
+//Enunciado:
+/*
+
+-- Programa Bom Dia! --
+
+Este programa trabalha com o conceito de varável. Nele o usuário vai passar uma informação para o programa,
+o programa guarda em uma variável e devolve uma resposta baseada nesta varável. O desafio é o seguinte,
+escreve um código que recebe o nome de uma pessoa e escreve "Bom Dia!".
+Por exemplo, se a entrada do programa for "João" o sua resposta deve ser "Bom Dia João!".
+
+Exemplo de entrada:
+Jonas
+
+Exemplo de saída:
+Bom Dia Jonas!
+
+Exemplo de entrada:
+Maria
+
+Exemplo de saída:
+Bom Dia Maria!
+
+11551 alunos realizaram a atividade.
+Autor(a): orivaldo@gmail.com
+
+*/
+
+let nome = prompt("Qual é o seu nome?");
+
+console.log("Bom Dia " + nome + "!");
