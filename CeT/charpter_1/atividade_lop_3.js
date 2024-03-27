6993d3ff4934f79644a44824ff0d5f4098fca5cf
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-03-27 14:59:51 -0300

    Create atividade_lop_3.js

---
+++ CeT/charpter_1/atividade_lop_3.js
@@ -0,0 +1,24 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 15/03/2024. ECT - UFRN
+
+//Enunciado:
+/*
+
+-- Introdução Soma --
+
+A soma de dois números é realizada pelo operador " + ". Escreva um código para somar os seguintes números:
+10 + 13, 125 + 373 e 4235 + 8962. Use o comando "alert( ... )" para mostrar o resultado.
+
+Exemplo de saída:
+
+23 498 13197
+
+13438 alunos realizaram a atividade.
+Autor(a): orivaldo@gmail.com
+
+*/
+
+let soma1 = 10 + 13;
+let soma2 = 125 + 373;
+let soma3 = 4235 + 8962;
+
+console.log(soma1 + " " + soma2 + " " + soma3);
