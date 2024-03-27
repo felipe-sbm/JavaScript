28e7ee0ba1993e9798eb87194b8c824228c2d84f
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-03-27 14:57:12 -0300

    Create atividade_lop_2.js

---
+++ CeT/charpter_1/atividade_lop_2.js
@@ -0,0 +1,26 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 15/03/2024. ECT - UFRN
+
+//Enunciado:
+/*
+
+-- Calculando expressões - parte 1 --
+
+Faça um programa que cria um conjunto de variáveis e considere ela com os seguintes valores: a = 1.5, c = 2, d = 3.
+Implemente um programa que determine e exiba o valor da seguinte expressão (lembrando de usar os parênteses):
+
+a×(c+d)
+
+Exemplo de saída:
+7.5
+
+11395 alunos realizaram a atividade.
+Autor(a): orivaldo@gmail.com
+
+*/
+
+let a = 1.5;
+let c = 2;
+let d = 3;
+var calculo = a * (c+d);
+
+console.log(calculo);
