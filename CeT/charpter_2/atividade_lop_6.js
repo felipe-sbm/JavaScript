3bc48f547f9d7047c99927f79ead9cae99f80453
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-16 08:26:15 -0300

    Update atividade_lop_6.js

--- CeT/charpter_2/atividade_lop_6.js
+++ CeT/charpter_2/atividade_lop_6.js
@@ -3,7 +3,7 @@
 //Enunciado:
 /*
 
-Convertendo de metros para polegadas
+-- Convertendo de metros para polegadas --
 
 Escreva um programa que recebe um número real referente a uma medida em metros e imprima esta medida em: polegadas.
 Considere que uma polegada equivale à 0.0254 metros. Na hora de imprimir a resposta siga o seguinte modelo: 
