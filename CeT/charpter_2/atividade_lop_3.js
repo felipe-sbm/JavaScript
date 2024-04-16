8bec99b095e50fcbdca857966c62079585782e20
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-16 08:09:13 -0300

    Create atividade_lop_3.js

---
+++ CeT/charpter_2/atividade_lop_3.js
@@ -0,0 +1,27 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 21/03/2024. ECT - UFRN
+
+//Enunciado:
+/*
+
+-- Calculando expressões - parte 4 --
+
+Agora, considerando os seguintes valores para as variáveis a = 1.5, b = 4, c = 2, d = 3, e = 1.2 e f = 4.3,
+implemente um programa que determine e exiba os valores da seguinte expressão (fique atento à precedência de operadores):
+
+b×(e+f) / a×(c+d)
+
+9021 alunos realizaram a atividade.
+Autor(a): orivaldo@gmail.com
+
+*/
+
+let a = 1.5;
+let b = 4;
+let c = 2;
+let d = 3;
+let e = 1.2;
+let f = 4.3;
+
+var calculo = (a * (c + d)) /(b* (e + f));
+
+console.log(calculo);
