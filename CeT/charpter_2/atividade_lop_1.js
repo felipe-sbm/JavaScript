e624f2060ceed4f8f56a186030e9a8727da31988
Felipe Samuel <felipesbm2005@gmail.com>
2024-04-16 08:03:46 -0300

    Atividade de LOP

---
+++ CeT/charpter_2/atividade_lop_1.js
@@ -0,0 +1,25 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 21/03/2024. ECT - UFRN
+
+//Enunciado:
+/*
+
+-- Calculando expressões - parte 2 --
+
+Agora vamos fazer uma pequena alteração na forma da expressão. Considerando os seguintes
+valores para as variáveis a = 1.5, c = 2, d= 3. Implemente um programa que determine e exiba
+o valor das seguinte expressão (note que ela não tem os parênteses):
+
+ a×c+d   
+
+9900 alunos realizarama a atividade.
+
+Autor(a): orivaldo@gmail.com
+
+*/
+let a = 1.5;
+let c = 2;
+let d = 3;
+
+var calculo = a * c + d;
+
+console.log(calculo);
