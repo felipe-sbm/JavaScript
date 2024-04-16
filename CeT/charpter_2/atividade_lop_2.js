136bee653be3918024893b9711521daa3b8017e1
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-16 08:06:43 -0300

    Create atividade_lop_2.js

---
+++ CeT/charpter_2/atividade_lop_2.js
@@ -0,0 +1,26 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 21/03/2024. ECT - UFRN
+
+//Enunciado:
+/*
+
+-- Calculando expressões - parte 3 --
+
+Você viu que a ausência de parêntesis muda a o resultado da equação. Sem eles, as operações e multiplicação
+são resolvidas primeiro. Só depois é resolvida a soma. Agora considere os seguintes valores para as variáveis
+a = 1.5, b = 4, c = 2, d = 3. Implemente um programa que determine e exiba os valores da seguinte expressão:
+
+a×(c+d) / b
+​
+8231 alunos realizaram a atividade.
+Autor(a): orivaldo@gmail.com
+
+*/
+
+let a = 1.5;
+let b = 4;
+let c = 2;
+let d = 3;
+
+var calculo = a * (c + d)/b;
+
+console.log(calculo);
