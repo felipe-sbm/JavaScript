993222123114f4b3b208d0266584b630ce90be3f
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-21 23:44:15 -0300

    Create atividade_lop_06.js

---
+++ CeT/charpter_04/atividade_lop_06.js
@@ -0,0 +1,11 @@
+//Atividade de Lógica da programação, Feita por: Felipe SBM, data: 05/04/2024. ECT - UFRN
+
+let num = parseInt(prompt("Insira um valor:"));
+
+if (num == 20) {
+    alert("igual");
+} if (num > 20) {
+    alert("maior");
+} if (num < 20) {
+    alert("menor");
+}
