7ef73ff64972ad9568f7b5aea0c7cd34dee4a456
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-19 23:33:25 -0300

    Update prova-lop-01.js

--- CeT/prova-lop-01.js
+++ CeT/prova-lop-01.js
@@ -1,26 +1,35 @@
 // Atividade de lógica da programação, 11/03/2024, Escola de ciência & tecnologia
+
 /* -- Caixa Eletrônico --
 
-Escreva um programa que lê um valor inteiro positivo e exibe a quantidade mínima de cédulas a qual o valor pode ser trocado. Considere que as cédulas disponíveis possuem os valores: 200, 100, 50, 20, 10, 5, 2, 1.
+Escreva um programa que lê um valor inteiro positivo e exibe a quantidade mínima de cédulas
+a qual o valor pode ser trocado. Considere que as cédulas disponíveis possuem os valores:
+                            ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+                            | 200, 100, 50, 20, 10, 5, 2, 1. |
+                            ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
 
-Exemplo de entrada	Exemplo de saída
-576	2 de 200
-1 de 100
-1 de 50
-1 de 20
-0 de 10
-1 de 5
-0 de 2
-1 de 1
-11257	56 de 200
-0 de 100
-1 de 50
-0 de 20
-0 de 10
-1 de 5
-1 de 2
-0 de 1
-364
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+| Exemplo de entrada: | Exemplo de saída: |
+|---------------------|-------------------|
+| 576                 | 2 de 200          |
+|                     | 1 de 100          |
+|                     | 1 de 50           |
+|                     | 1 de 20           |
+|                     | 0 de 10           |
+|                     | 1 de 5            |
+|                     | 0 de 2            |
+|                     | 1 de 1            |
+|---------------------|-------------------|
+| 11257               | 56 de 200         |
+|                     | 0 de 100          |
+|                     | 1 de 50           |
+|                     | 0 de 20           |
+|                     | 0 de 10           |
+|                     | 1 de 5            |
+|                     | 1 de 2            |
+|                     | 0 de 1            |
+´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+364 Alunos visualizaram a atividade
 Autor(a): thalesaguiar21@gmail.com */
 
 let grana = parseInt(prompt("Insira o valor que quer receber:"));
