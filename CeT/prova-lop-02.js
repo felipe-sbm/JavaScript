2a78623e81d14d093bce1504edcc102a44ebadcf
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-19 23:38:08 -0300

    Update prova-lop-02.js

--- CeT/prova-lop-02.js
+++ CeT/prova-lop-02.js
@@ -1,13 +1,20 @@
-// Atividade de lógica da programação, 11/03/2024, Escola de ciência & tecnologia
+// Atividade de lógica da programação, 17/06/2024, Escola de ciência & tecnologia
 
 /* -- Números de Lucas --
 
-Os números de Lucas são uma sequência de números inteiros onde o iii​-ésimo termo é dado por L(i)=L(i−1)+L(i−2)L(i) = L(i-1) + L(i-2)L(i)=L(i−1)+L(i−2) para i≥2i \geq 2i≥2​, sendo L(0)=2L(0)=2L(0)=2​ e L(1)=1L(1) = 1L(1)=1​. Escreva um programa que lê um inteiro positivo iii​ e imprime o iii​ primeiros termos da sequência de Lucas.
+Os números de Lucas são uma sequência de números inteiros onde o i​-ésimo termo é dado
+por:              L(i)=L(i−1)+L(i−2)L(i) = L(i-1) + L(i-2)L(i)=L(i−1)+L(i−2) para i≥2i
+e 2i≥2​, sendo:    L(0)=2L(0)=2L(0)=2​ e L(1)=1L(1) = 1L(1)=1​.
+Escreva um programa que lê um inteiro positivo i​ e imprime o i primeiros termos da sequência de Lucas.
 
-Exemplo de entrada	Exemplo de saída
-4	2,1,3,4
-10	2,1,3,4,7,11,18,29,47,76
-287
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+| Exemplo de entrada: | Exemplo de saída:        |
+|---------------------|--------------------------|
+| 4                   | 2,1,3,4                  |
+|---------------------|--------------------------|
+| 10                  | 2,1,3,4,7,11,18,29,47,76 |
+´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+287 Alunos visualizaram a atividade.
 Autor(a): thalesaguiar21@gmail.com */
 
 let Inumero = Math.abs(parseInt(prompt("Eaí essa galera, informe o i-nésimo termo para o programa:")));
