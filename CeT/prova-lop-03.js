e5a8e651ef58e6f52351b2f8c5b8728be5285b76
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-19 23:44:11 -0300

    Update prova-lop-03.js

--- CeT/prova-lop-03.js
+++ CeT/prova-lop-03.js
@@ -2,29 +2,34 @@
 
 /* -- Conta sorrisos --
 
-Ao realizar uma análise de mensagens do twitter, você nota que existe uma certa frequência de carinhas tristes para uma quantidade de sorrisos. Portanto, você precisa escrever um programa que conte quantos sorrisos existem para cada 3 carinhas tristes. Logo, seu programa deve parar a leitura e a contagem sempre que encontrar 3 caras tristes, e continuar caso contrário. Ao final, seu programa deve exibir a contagem de sorrisos, caretas, e mensagens desconhecidas.
+Ao realizar uma análise de mensagens do twitter, você nota que existe uma certa frequência
+de carinhas tristes para uma quantidade de sorrisos. Portanto, você precisa escrever um programa
+que conte quantos sorrisos existem para cada 3 carinhas tristes. Logo, seu programa deve parar a
+leitura e a contagem sempre que encontrar 3 caras tristes, e continuar caso contrário. Ao final,
+seu programa deve exibir a contagem de sorrisos, caretas, e mensagens desconhecidas. :-)
 
-SORRISOS: :)    :D     ;-D     :~)
-
-CARAS TRISTES: ;(    :<    :}    :]
-
-Exemplo de entrada	Exemplo de saída
-:)
-:<
-:}
-:D
-;-D
-;-D
-:~)
-;(	5 sorrisos
-3 caretas
-0 desconhecidos
-;(
-:<
-:}	0 sorrisos
-3 caretas
-0 desconhecidos
-218
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+| SORRISOS: :)    :D     ;-D     :~) |
+|------------------------------------|
+| CARAS TRISTES: ;(    :<    :}   :] |
+´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+,,.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+| Exemplo de entrada: | Exemplo de saída: |
+|---------------------|-------------------|
+| :)                  | 5 sorrisos        |
+| :<                  | 3 caretas         |
+| :}                  | 0 desconhecidos   |
+| :D                  |                   |
+| ;-D                 |                   |
+| ;-D                 |                   |
+| :~)                 |                   |
+| ;(                  |                   |
+|---------------------|-------------------|
+| ;(                  | 0 sorrisos        |
+| :<                  | 3 caretas         |
+| :}	                | 0 desconhecidos   |
+´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+218 Alunos visualizaram a atividade.
 Autor(a): thalesaguiar21@gmail.com */
 
 const carinhasF = [":)", ":D", ";-D", ":~)"];
